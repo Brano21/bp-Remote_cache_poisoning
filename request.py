2f52b1f2fad2c31bd196201bba47d5fe5dc4ec90
Brano21 <56072216+Brano21@users.noreply.github.com>
2022-04-29 08:58:46 +0200

    DNS_Remote-cache-poisoning-attack

--- request.py
+++ request.py
@@ -4,8 +4,8 @@ from scapy.all import *
 Qdsec = DNSQR(qname='aaaaa.example.com')
 dns = DNS(id=0xAAAA, qr=0, qdcount=1, ancount=0, nscount=0,
 arcount=0, qd=Qdsec)
-ip = IP(dst='10.10.30.7', src='10.10.30.6')
-udp = UDP(dport=53, sport=9999, chksum=0)
+ip = IP(dst='********', src='********')
+udp = UDP(dport=*****, sport=*****, chksum=0)
 request = ip/udp/dns
 with open ('ip_req.bin', 'wb') as f:
     f.write(bytes(request))
